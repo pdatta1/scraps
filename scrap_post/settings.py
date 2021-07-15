96f4b02eb38b9ca8d997b2fc1f9aed8a767c4c88
pdatta1 <patrickattabaah16@gmail.com>
2021-07-15 00:01:09 -0400

    sd

--- scrap_post/settings.py
+++ scrap_post/settings.py
@@ -22,9 +22,11 @@ TEMPLATES_DIRS = [os.path.join(PROJECT_PATH, 'templates/')]
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_DIR, 'static'),
+)
 
 
 # Quick-start development settings - unsuitable for production
